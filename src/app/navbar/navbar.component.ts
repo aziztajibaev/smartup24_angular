76d2baf131c89397510e00afa7912714985bccf6
unknown <azizbek.tadjibaev@greenwhite.uz>
2024-04-11 18:49:33 +0500

    add navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+// bu component test sifatida yaratildi.
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
